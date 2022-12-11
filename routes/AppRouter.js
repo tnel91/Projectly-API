7f8905172fafd713a9a4d8cca758866a9413baa1
Travis Nelson <tjnel6691@gmail.com>
2022-12-10 21:41:54 -0500

    Added dependencies for auth

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,6 +3,8 @@ const controller = require('../controllers/AppController')
 
 router.get('/users', controller.getAllUsers)
 
+// router.post('/users', controller.createUser)
+
 router.get('/projects', controller.getPublicProjects)
 
 router.get('/projects/:projectId', controller.getProjectById)
