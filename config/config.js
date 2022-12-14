ca3194c8705a90f7cfd135efaed8dd475739c567
Travis Nelson <tjnel6691@gmail.com>
2022-12-14 14:53:50 -0500

    Fixed typo

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: 'DATABSE_URL',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
