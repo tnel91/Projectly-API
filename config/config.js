e4e2a01c172a38d156e543b0f182b4f8a3174ec0
Travis Nelson <tjnel6691@gmail.com>
2022-12-14 14:29:52 -0500

    Prepped for heroku deployment

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+module.exports = {
+  development: {
+    database: 'project_planner_development',
+    dialect: 'postgres'
+  },
+  test: {
+    database: 'project_planner_test',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABSE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    }
+  }
+}
--- test.js
+++
@@ -1,5 +0,0 @@
-const falso = require('@ngneat/falso')
-
-let res = falso.randSoonDate()
-
-console.log(res)
